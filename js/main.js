b6b9ab472737a0a1bb8897fad63a4ddb14856b84
kaenry <kaenry@163.com>
2014-06-10 22:29:49 +0800

    change Header URL, add imgs

--- js/main.js
+++ js/main.js
@@ -2,26 +2,18 @@ $(function($) {
 	$("#m-about").hover(function() {
 		$(".mega-menu, #about-bamboozle, #about-bamboozle div").css("display", "block");
 		$("#our-range, #our-range div").css("display", "none");
-		$("#gallery, #gallery div").css("display", "none");
 		$(".extras").css("display", "block");
 	});
 	$("#m-out").hover(function() {
 		$(".mega-menu, #our-range, #our-range div").css("display", "block");
 		$("#about-bamboozle, #about-bamboozle div").css("display", "none");
-		$("#gallery, #gallery div").css("display", "none");
 		$(".extras").css("display", "block");
 	});
-	$("#m-gallery").hover(function() {
-		$(".mega-menu, #gallery, #gallery div").css("display", "block");
-		$("#about-bamboozle, #about-bamboozle div").css("display", "none");
-		$("#our-range, #our-range div").css("display", "none");
-		$(".extras").css("display", "block");
-	})
 	$(".mega-menu").mouseleave(function() {
 		$(".mega-menu, .mega-menu .container>div").css("display", "none");
 	})
 	
-	$("nav .nav li:nth-child(n+4) a").hover(function() {
+	$("nav .nav li:nth-child(n+3) a").hover(function() {
 		$(".mega-menu, .mega-menu .container>div").css("display", "none");
 	});
 
