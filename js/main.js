141d1cbca653c7cd19a4df6624c167dbc21c9bd7
kaenry@163.com <kaenry@163.com>
2014-06-10 13:31:49 +0800

    add sliderman.js to slide homepage and move old slider

--- js/main.js
+++ js/main.js
@@ -24,5 +24,16 @@ $(function($) {
 	$("nav .nav li:nth-child(n+4) a").hover(function() {
 		$(".mega-menu, .mega-menu .container>div").css("display", "none");
 	});
+
+	effect = { left: true, fade: true, easing: 'pow2', order: 'rectangle', duration: 2000 };
+	var demoSlider_2 = Sliderman.slider({container: 'full-slider', width: 1600, height: 680, effects: [ effect ],
+		display: {
+			autoplay: 5000
+			// loading: {background: '#000000', opacity: 0.5, image: 'img/loading.gif'},
+			// buttons: {hide: true, opacity: 1, prev: {className: 'SliderNamePrev_2', label: ''}, next: {className: 'SliderNameNext_2', label: ''}},
+			// description: {hide: true, background: '#000000', opacity: 0.4, height: 50, position: 'bottom'},
+			// navigation: {container: 'SliderNameNavigation_2', label: '<img src="img/clear.gif" />'}
+		}
+	});
 	
 })
